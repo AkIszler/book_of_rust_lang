36661160c9bff207a83f2688974baa5abd866196
Anthony Iszler <aiszler29@gmail.com>
2023-08-07 22:46:32 -0700

    save

---
+++ data_stuff/target/debug/data_stuff.d
@@ -0,0 +1,1 @@
+/home/bsd/home/rust/book/data_stuff/target/debug/data_stuff: /home/bsd/home/rust/book/data_stuff/src/main.rs
---
+++ data_stuff/target/debug/deps/data_stuff-2903961f5ea97d06.d
@@ -0,0 +1,5 @@
+/home/bsd/home/rust/book/data_stuff/target/debug/deps/data_stuff-2903961f5ea97d06: src/main.rs
+
+/home/bsd/home/rust/book/data_stuff/target/debug/deps/data_stuff-2903961f5ea97d06.d: src/main.rs
+
+src/main.rs:
