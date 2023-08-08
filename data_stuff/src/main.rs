157954d56dbf8fbc6ec30ab45657674ef9d51869
Anthony Iszler <aiszler29@gmail.com>
2023-08-07 22:46:20 -0700

    more progress

---
+++ data_stuff/src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+
+let mut s = String::from("hello"); // string representation
+
+s.push_str(", people"); // pushing to string representation
+
+println!("{s}");
+
+let mut n = 5;
+
+while n > 0 {
+    s.push_str("!");
+    n -= 1;
+    }
+    println!("{s}"); // prints Hello people after adding 5 "!" characters
+
+    let mut count = String::from("count");
+
+}
+
